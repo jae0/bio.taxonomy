1b6fc7abcf123a0d765ec1365aa6d9dbb924de4a
Jae S Choi <choijaeseok@gmail.com>
2017-11-21 22:07:46 -0400

    cleanup

--- R/taxonomy.filter.taxa.r
+++ R/taxonomy.filter.taxa.r
@@ -2,7 +2,7 @@
   taxonomy.filter.taxa = function ( spcode, method=c("direct", "complete"), taxafilter="all", outtype="internalcodes" ) {
     out = data.frame(spcode=spcode, sortindex=1:length(spcode), keep=FALSE )
     if ( outtype %in% c( "spec_bio", "internalcodes", "parsimonious", "spec.parsimonious") ) outcode="spec.parsimonious"
-    if ( outtype %in% c( "spec", "bio.groundfishcodes") ) outcode ="spec"
+    if ( outtype %in% c( "spec", "aegiscodes") ) outcode ="spec"
     taxalist = taxonomy.codes( taxa=taxafilter,method=method, outcode=outcode )  # these are still groundfish codes
     ##taxalist = taxonomy.codes( method=taxafilter, outcode=outcode )  # making a change AMC for species composition
     f = which( is.finite( out$spcode ) )
